296f032692c3ee4f6735397554bdb02fd9fdae1b
dflippo <djf13@cornell.edu>
2013-12-22 02:39:53 -0500

    Adding theme credit to footer.php
    Added a small bit of text to help track how many websites adopt the
    theme

--- footer.php
+++ footer.php
@@ -15,11 +15,13 @@
       echo '<p>&nbsp;</p>';
       endif; ?>
 		<div class="site-info">
+  	<?php do_action( 'uu2014_credits' ); ?>
     <p>Copyright &copy; <?php echo date("Y"); ?> : 
 <a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
 <span class="sep"> | </span>
+<?php printf( __( 'WordPress Theme : %1$s', 'uu2014' ), '<a href="http://www.faithandreason.dreamhosters.com/" rel="designer">UU2014</a>' ); ?>      
+<span class="sep"> | </span>
 <a href="<?php echo get_admin_url(); ?>" title="Login">Login</a></p>
-			<?php do_action( 'uu2014_credits' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
